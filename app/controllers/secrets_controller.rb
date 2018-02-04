ad10724ce0ede752848de6f1c65605116ee49ab5
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-04 16:53:13 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,3 +1,13 @@
 class SecretsController < ApplicationController
+  before_action :require_login
+
+  def show
+
+  end
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id 
+  end
+  
 
 end
