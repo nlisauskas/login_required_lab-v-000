8efddbe24b41bc1e008b02976f2a12263cdbe4b6
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-04 17:23:09 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name].nil? || params[:name] = ""
+    if params[:name].blank? || params[:name] = ""
       redirect_to '/login'
     else
       session[:name] = params[:name]
